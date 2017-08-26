c99c968e079e7d24bcf1a2cdd42ab33247947456
andrey sushentsov <p1oneer8527@gmail.com>
2017-08-26 11:45:56 +0500

    fix

--- core/Database.php
+++ core/Database.php
@@ -7,6 +7,6 @@ class Database
 
   function __construct()
   {
-    $this->connect = new mysqli('localhost','root','6505599930','homework_4');
+    $this->connect = new mysqli('localhost','root','111111','homework_4');
   }
 }
